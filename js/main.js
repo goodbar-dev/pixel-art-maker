9342452178e42425e25765c6623640b8b93a5c69
Joe Widi <jwidi@outlook.com>
2017-11-06 15:59:27 -0600

    Fully Functional/validated

--- js/main.js
+++ js/main.js
@@ -1,22 +1,35 @@
+function toggleBackground() {
+  const color = $(this).css('background-color');
+
+  if (color == 'transparent' || color == 'rgb(0, 0, 0)') {
+    $(this).css('background-color', $('#btnColor').val());
+  }
+  else {
+    $(this).css('background-color', 'transparent');
+  }
+}
+
 function makeGrid() {
   event.preventDefault();
 
   const height = $('#grid-height').val();
   const width = $('#grid-width').val();
+  const boxWidth = width * 17;  //box width + left/right border
   let h;
   let w;
-  let content = "";
 
-  $('.grid').attr('width', width * 15);
+  $('.grid').css('max-width', boxWidth);
+  $('.grid').css('min-width', boxWidth);
+  $('.grid').empty();
 
   for (h = 1; h <= height; h++) {
     for (w = 1; w <= width; w++) {
-        content = content + "<div class=\"box " + h + "-" + w + "\"></div>";
+      $('.grid').append('<div class=\"box\"></div>');
     }
-    $('.grid').append(content);
-    content = "";
   }
-};
+
+  $('.box').on('click', toggleBackground);
+}
 
 $(document).ready(function() {
   $('#btnSubmit').on('click', makeGrid);
